2bf16a769110ab6c1178539491d2eb44d1a5e39f
Dmitry Dmitriev <51149339+DmitrievDM@users.noreply.github.com>
2020-04-09 18:11:26 +0500

    Create project and some function

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Объявление переменных
+            String inputString = "";
+            
+            Console.WriteLine("Добро пожаловать на карантин! \nСегодня мы будем делать (велосипед) калькулятор!");
+            
+            inputString = Console.ReadLine();
+
+            Console.WriteLine(inputString);
+            Console.ReadLine();
+
+            
+        }
+
+        public static string GetShortString(string arg) {
+            String paterrn = @"/(\()((\d+\.\d+)|(\d+))((\+|\-|\*|\/)((\d+\.\d+)|(\d+)))*(\))/";    //выдёргиваем часть выражения в скобках
+            String substring = Regex.Match(arg, paterrn).Value;
+            if (String.IsNullOrEmpty(substring))
+            {
+
+            }
+            else
+            {
+                List<String> args = new List<String>();
+                for (var m = Regex.Match(substring, @"/((\d+\.\d+)|(\d+))|(\+|\-|\*|\/)/"); m.Success; m = m.NextMatch()) //записываем все числа и знаки в список
+                {
+                    args.Add(m.Value);
+                }
+            }
+            return arg;
+        }
+
+        public static float GetResult(float arg1, float arg2, string oper, float result = 0)    //вычисление операций между двумя переменными
+        {
+            switch (oper)
+            {
+                case "+":
+                    Console.WriteLine("Складываем " + arg1 + " и " + arg2);
+                    result = arg1 + arg2;
+                    break;
+                case "-":
+                    Console.WriteLine("Складываем " + arg1 + " и " + arg2);
+                    result = arg1 - arg2;
+                    break;
+                case "*":
+                    Console.WriteLine("Складываем " + arg1 + " и " + arg2);
+                    result = arg1 * arg2;
+                    break;
+                case "/":
+                    Console.WriteLine("Складываем " + arg1 + " и " + arg2);
+                    result = arg1 / arg2;
+                    break;
+                default:
+                    Console.WriteLine("Ну и какая тут операция между " + arg1 + " и " + arg2 + "?");
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
